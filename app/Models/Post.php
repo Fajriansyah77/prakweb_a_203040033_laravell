c424d0a436140e13f33789718237249d9e55c285
fajrlansyah <easieryousay@gmail.com>
2022-11-26 18:49:35 +0700

    Video #18 - Create Post Form

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
     protected $guarded = ['id'];
     protected $with = ['author', 'category'];
 
@@ -43,4 +45,12 @@ class Post extends Model
         return 'slug';
     }
 
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
